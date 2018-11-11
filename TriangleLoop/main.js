730d53a065f1088fcc4f4cc4025d0629c7ba309a
Rebecca Rothschild <39253970+rebeccachild74@users.noreply.github.com>
2018-11-11 13:51:55 -0800

    Finished miniprogram

--- TriangleLoop/main.js
+++ TriangleLoop/main.js
@@ -1,3 +1,23 @@
 /*
  * Auto-generated content from the Brackets New Project extension.  Enjoy!
  */
+
+
+
+function testFunction() {
+    
+    var count;
+    var testString;
+    
+    for (count = 0; count < 7; count = count + 1) {
+        testString += "*";
+        console.log(testString);
+        
+    }
+    document.getElementById("test").innerHTML = testString; // just testing
+    return testString;
+}
+
+ 
+
+console.log(testFunction());
