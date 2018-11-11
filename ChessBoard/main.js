8ec063bc489220c50c052c4650fc89703f27637b
Rebecca Rothschild <39253970+rebeccachild74@users.noreply.github.com>
2018-11-11 15:23:34 -0800

    working

--- ChessBoard/main.js
+++ ChessBoard/main.js
@@ -3,16 +3,16 @@ var dimension = 8;
 function chessBoardGenerator(dimension){
  
     var i;
-    var j;
+    //var j;
     
     var str1;
-    var str2;
+    //var str2;
     
     for(i = 0; 0 < dimension/2; i++){ //fill str1
         str1 += "# ";
     }
     
-    console.log("str1");
+    console.log(str1);
 
 }// end chessBoardGenerator
 
