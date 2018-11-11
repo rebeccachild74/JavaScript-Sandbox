d087f8f5aeb882af47a7a5e177259a03827d2cb4
Rebecca Rothschild <39253970+rebeccachild74@users.noreply.github.com>
2018-11-11 15:18:36 -0800

    working on program, having some IDE issues

---
+++ ChessBoard/main.js
@@ -0,0 +1,19 @@
+var dimension = 8;
+
+function chessBoardGenerator(dimension){
+ 
+    var i;
+    var j;
+    
+    var str1;
+    var str2;
+    
+    for(i = 0; 0 < dimension/2; i++){ //fill str1
+        str1 += "# ";
+    }
+    
+    console.log("str1");
+
+}// end chessBoardGenerator
+
+chessBoardGenerator(dimension);
